efb2d85ea9989dcf535c68acba6b45e95b486fbd
Jahidul Pabel Islam <jahidul.pabel.islam@hotmail.com>
2017-11-22 20:52:12 +0000

    create a list of movies and display them.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import { Movie } from './movie';
+
+const MOVIES: Movie[] = [
+    { id: 1, name: 'The Fast and the Furious', genre: 'Honda' , year: 2001},
+    { id: 2, name: 'Harry Potter', genre: 'Honda' , year : 2000},
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Movies Collection';
+  movies = MOVIES;
 }
