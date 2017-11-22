234c37990b9aba05d0ccbdbd86ec285e611ded16
Jahidul Pabel Islam <jahidul.pabel.islam@hotmail.com>
2017-11-22 22:12:19 +0000

    commit all work done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { MoviesComponent } from './components/movies/movies.component';
 
+
+import { MoviesService } from './services/movie.service';
+
+const appRoutes: Routes = [
+    { path: 'movie/:id', component: MoviesComponent },
+    {
+        path: 'movies',
+        component: MoviesComponent,
+        data: { title: 'Movies List' }
+    },
+    { path: '',
+        redirectTo: '/movies',
+        pathMatch: 'full'
+    },
+    { path: '**', component: MoviesComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
