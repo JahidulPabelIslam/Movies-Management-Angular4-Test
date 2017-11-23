365075c572f2e8e2625570de11022641b3467b05
Jahidul Pabel Islam <jahidul.pabel.islam@hotmail.com>
2017-11-23 21:33:03 +0000

    Create a movies service

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@angular/core';
 
+import { Movie } from '../classes/movie';
+
 @Injectable()
 export class MoviesService {
 
-  constructor() { }
+    private movies: Movie[];
+    private nextId: number;
+
+  constructor() {
+    this.movies = [
+        new Movie(1, 'The Fast and the Furious', 'Honda', 2001),
+        new Movie(2, 'Harry Potter', 'Honda' , 2000)
+    ];
+
+     this.nextId = 3;
+  }
+
+    public addMovie(name: string, genre: string, year: number): void {
+        const movie = new Movie(this.nextId, name, genre, year);
+        this.movies.push(movie);
+        this.nextId++;
+    }
+
+    public getMovies(): Movie[] {
+        return this.movies;
+    }
+
+    public removeMovie(id: number): void {
+        this.movies = this.movies.filter((movie) => movie.id !== id);
+    }
 
 }
